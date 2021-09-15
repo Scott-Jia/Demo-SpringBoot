44962fdf7478ef0fcb56a6ca8ed229b5ffe058f2
Scott-Jia <bojia2020@gmail.com>
2021-09-14 23:46:01 -0500

    update changes

--- src/main/java/com/projectbo/demo/DemoApplication.java
+++ src/main/java/com/projectbo/demo/DemoApplication.java
@@ -3,11 +3,17 @@ package com.projectbo.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.SQLOutput;
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DemoApplication.class, args);
+
+		// System.out.println("test running");
+
 	}
 
 }
