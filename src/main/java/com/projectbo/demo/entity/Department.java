b54769acc82c4954070679da4b213719a97dab21
Scott-Jia <bojia2020@gmail.com>
2021-12-30 08:05:43 -0600

    add Lombok dependency and plugin

--- src/main/java/com/projectbo/demo/entity/Department.java
+++ src/main/java/com/projectbo/demo/entity/Department.java
@@ -1,5 +1,6 @@
 package com.projectbo.demo.entity;
 
+import lombok.Data;
 import net.bytebuddy.dynamic.loading.InjectionClassLoader;
 
 import javax.persistence.Entity;
@@ -9,6 +10,7 @@ import javax.persistence.Id;
 import javax.validation.constraints.*;
 
 @Entity
+@Data
 public class Department {
 
     @Id
@@ -23,55 +25,55 @@ public class Department {
     private String deptAddress;
     private String deptCode;
 
-    public Department() {
-    }
-
-    public Department(Long deptId, String deptName, String deptAddress, String deptCode) {
-        this.deptId = deptId;
-        this.deptName = deptName;
-        this.deptAddress = deptAddress;
-        this.deptCode = deptCode;
-    }
-
-    @Override
-    public String toString() {
-        return "Department{" +
-                "deptId=" + deptId +
-                ", deptName='" + deptName + '\'' +
-                ", deptAddress='" + deptAddress + '\'' +
-                ", deptCode='" + deptCode + '\'' +
-                '}';
-    }
-
-    public Long getDeptId() {
-        return deptId;
-    }
-
-    public void setDeptId(Long deptId) {
-        this.deptId = deptId;
-    }
-
-    public String getDeptName() {
-        return deptName;
-    }
-
-    public void setDeptName(String deptName) {
-        this.deptName = deptName;
-    }
-
-    public String getDeptAddress() {
-        return deptAddress;
-    }
-
-    public void setDeptAddress(String deptAddress) {
-        this.deptAddress = deptAddress;
-    }
-
-    public String getDeptCode() {
-        return deptCode;
-    }
-
-    public void setDeptCode(String deptCode) {
-        this.deptCode = deptCode;
-    }
+//    public Department() {
+//    }
+//
+//    public Department(Long deptId, String deptName, String deptAddress, String deptCode) {
+//        this.deptId = deptId;
+//        this.deptName = deptName;
+//        this.deptAddress = deptAddress;
+//        this.deptCode = deptCode;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Department{" +
+//                "deptId=" + deptId +
+//                ", deptName='" + deptName + '\'' +
+//                ", deptAddress='" + deptAddress + '\'' +
+//                ", deptCode='" + deptCode + '\'' +
+//                '}';
+//    }
+//
+//    public Long getDeptId() {
+//        return deptId;
+//    }
+//
+//    public void setDeptId(Long deptId) {
+//        this.deptId = deptId;
+//    }
+//
+//    public String getDeptName() {
+//        return deptName;
+//    }
+//
+//    public void setDeptName(String deptName) {
+//        this.deptName = deptName;
+//    }
+//
+//    public String getDeptAddress() {
+//        return deptAddress;
+//    }
+//
+//    public void setDeptAddress(String deptAddress) {
+//        this.deptAddress = deptAddress;
+//    }
+//
+//    public String getDeptCode() {
+//        return deptCode;
+//    }
+//
+//    public void setDeptCode(String deptCode) {
+//        this.deptCode = deptCode;
+//    }
 }
